7ef4889357f4901b5b2b1329bfab2036ea63afd9
Nirae3 <ardiarkel3@gmail.com>
2025-11-12 10:50:51 -0500

    4_system_integration skeleton function

--- 4_system_integration.py
+++ 4_system_integration.py
@@ -16,3 +16,27 @@ Output: The final working Etch-a-Sketch-style robot arm program.
 
 """
 ###############################################################################
+
+def initialize_system() -> None: #type: ignore
+    """
+    initialise the system: servos, ADC pins
+    """
+    pass
+
+def calibrate_servos() -> None: #type: ignore
+    """
+    move the phisical arm around the paper and calculate for the ranges where it can go
+    perform basic callibrations
+    for that, call 1_servo_control to perform the callibrations within 10 degrees 
+    """
+    pass
+
+def update_arm_position() -> dict[str, float] #type: ignore
+    """
+    read inputs and update servo positions
+    shoulder, elbow = get_target_angles()
+    move_servo("shoudler", shoulder)
+    """
+    pass
+
+def main_loop() ->  None #type: ignore
