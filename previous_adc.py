5c21ff25401e27e9d1f9ccdff989170aff945565
Nirae3 <ardiarkel3@gmail.com>
2025-11-27 16:28:39 -0500

    pair based proj, external adc and how it's used

--- previous_adc.py
+++ previous_adc.py
@@ -1,3 +1,16 @@
+from machine import PWM, UART, Pin, ADC, I2C
+import time
+#from adc1 import adc, ADS1015_PWM
+from ads1x15 import ADS1015
+
+#ADS1015 is 12-bit. reads analog voltages and turns them into digital values
+ADS1015_PWM = 2
+
+#initializing i2c bus
+i2c = I2C(1, sda=Pin(14), scl=Pin(15))
+external_adc = ADS1015(i2c, 0x48, 1) #(I2C bus, I2C address, parameter)
+
+
 ADS_MIN_RAW = 9
 #converting raw ADS1015 reading to 16-bit value
 SCALING_FACTOR = 41.5 # SCALING_FACTOR = 65535 / (  (ADS_MAX)/32  -  (ADS_MIN)/32   ) <- calculated as per
@@ -21,10 +34,7 @@ while True:
         adjusted_raw = max(0, measured_signal_value_raw - ADS_MIN_RAW) #setting the maximum signal that can be sent through at a time
         measured_signal_value = int(adjusted_raw * SCALING_FACTOR) #turning th analog values to an integer
         measured_signal_value = min(measured_signal_value, 65535) ##setting the minimum signal that can be sent through at a time
-        
-        measured_uart_value = read_uart_line(uart) 
-        
-        #reading the value gotten through uart
 #        print(f"desired raw pwm signal: {desired_pot_value: <30} | value i got back from partenr: {uart.read_uart_line(str(measured_signal_value_raw).encode())}")
         print(f"Desired raw PWM: {desired_pot_value :<10} | Scaled version Of what cae back: {measured_uart_value * 40}" ) 
-
+        time.sleep(0.5)
+        
